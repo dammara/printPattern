cbfce1e69dda9ec8b78927399aeab5adc22959da
dammara <56162593+dammara@users.noreply.github.com>
2021-09-13 09:45:36 -0400

    Add files via upload

---
+++ bonus.py
@@ -0,0 +1,17 @@
+# Markhus Dammar
+# 13 September 2021
+# Bonus for 30 pts, instance 1
+
+print("""
+     +
+  +     +
++         +
+  +  +  +
+   +   +
+     *
+  *     *
+*         *
+  *  *  *
+   *   *
+      
+""")
